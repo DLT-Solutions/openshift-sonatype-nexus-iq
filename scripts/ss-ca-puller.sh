10f0d4d887f77caaab804d120f246fdada6a2b37
Ken Moini <ken@kenmoini.com>
2019-11-04 23:21:13 -0600

    thinkigotthestufferstuffed

--- scripts/ss-ca-puller.sh
+++ scripts/ss-ca-puller.sh
@@ -14,27 +14,28 @@ function testCert() {
   set -e
 }
 
-function determineJavaHome() {
-  # Call internal REST API for support file
-  mkdir /tmp/jhds
-  curl -u admin:admin123 --output /tmp/jhds/support.zip localhost:8070/rest/support
-  cd /tmp/jhds
-  ## img doesnt have unzip so we use jar lol
-  jar xvf support.zip
-  cd support-*/info
-  JHOME=$(grep -Po '"java.home" :.*?[^\\]",' sysinfo.json | cut -d ':' -f 2 | tr -d '[:space:]' | sed 's/"//g')
-  rm -rf /tmp/jhds
-  return ${JHOME%?}
-}
+## Determine Java Home lol
+# Call internal REST API for support file
+mkdir /tmp/jhds
+curl -sS -u admin:admin123 --output /tmp/jhds/support.zip localhost:8070/rest/support
+cd /tmp/jhds
+## img doesnt have unzip so we use jar lol
+jar xf support.zip
+cd support-*/info
+TJHOME=$(grep -Po '"java.home" :.*?[^\\]",' sysinfo.json | cut -d ':' -f 2 | tr -d '[:space:]' | sed 's/"//g')
+rm -rf /tmp/jhds
+JHOME=${TJHOME%?}
+
+cd $JHOME
 
 ## check for custom keystore
-CUSTOM_TRUSTSTORE=$JENKINS_HOME/.cacerts
+CUSTOM_TRUSTSTORE="/sonatype-work/.cacerts"
 
 if [ ! -f "$CUSTOM_TRUSTSTORE/cacerts" ]; then
     echo "No local keystore found, creating..."
     mkdir -p $CUSTOM_TRUSTSTORE
     JHOME=determineJavaHome
-    cp $JHOME/jre/lib/security/cacerts $CUSTOM_TRUSTSTORE
+    cp $JHOME/lib/security/cacerts $CUSTOM_TRUSTSTORE
 fi
 
 chmod +w $CUSTOM_TRUSTSTORE/cacerts
@@ -47,7 +48,6 @@ do
   echo "Pulling SSL certificate for ${CERT}..."
   FILENAME=${CERT//":"/".p"}
   CERTNAME=${FILENAME//"."/"-"}
-  #echo "Q" | openssl s_client -connect ${CERT} 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/$FILENAME.pem
   keytool -printcert -rfc -sslServer ${CERT} > /tmp/$FILENAME.pem
   echo "Checking if certificate exists in keystore..."
   testCert $CUSTOM_TRUSTSTORE/cacerts $CERTNAME
